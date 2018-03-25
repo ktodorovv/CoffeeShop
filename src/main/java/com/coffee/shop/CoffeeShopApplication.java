081f806b4cdb01d593093fa9094864b647aebb4e
ktodorovv <kiril.todorov@gmail.com>
2018-03-25 11:37:59 +0300

    Changed package name

--- src/main/java/com/coffee/shop/CoffeeShopApplication.java
+++ src/main/java/com/coffee/shop/CoffeeShopApplication.java
@@ -1,4 +1,4 @@
-package com.coffee.shop.CoffeeShop;
+package com.coffee.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
